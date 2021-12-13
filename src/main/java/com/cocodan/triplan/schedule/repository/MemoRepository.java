d4fac624269322f486f354f78cd5d6b3811001cb
HwiNyeon Kim <z2b23173us@gmail.com>
2021-12-13 16:46:31 +0900

    [TP-60] Feature: 유효성 검증 코드 리팩토링

--- src/main/java/com/cocodan/triplan/schedule/repository/MemoRepository.java
+++ src/main/java/com/cocodan/triplan/schedule/repository/MemoRepository.java
@@ -2,6 +2,15 @@ package com.cocodan.triplan.schedule.repository;
 
 import com.cocodan.triplan.schedule.domain.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    boolean existsByIdAndScheduleId(Long id, Long scheduleId);
+
+    @Query(value = "SELECT * FROM memo WHERE schedule_id = :scheduleId", nativeQuery = true)
+    List<Memo> findByScheduleId(Long scheduleId);
 }
--- src/main/java/com/cocodan/triplan/schedule/repository/VotingRepository.java
+++ src/main/java/com/cocodan/triplan/schedule/repository/VotingRepository.java
@@ -2,6 +2,15 @@ package com.cocodan.triplan.schedule.repository;
 
 import com.cocodan.triplan.schedule.domain.Voting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 public interface VotingRepository extends JpaRepository<Voting, Long> {
+
+    boolean existsByIdAndScheduleId(Long id, Long scheduleId);
+
+    @Query(value = "SELECT * FROM voting WHERE schedule_id = :scheduleId", nativeQuery = true)
+    List<Voting> findByScheduleId(Long scheduleId);
 }
