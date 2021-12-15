10826df8784eed94dd3593d5ba1069e35f6ef563
neilsonT <neils110@gmail.com>
2021-12-15 16:55:49 +0900

    [TP-80] Feature : 비즈니스 로직 파일로 로그 남기기

---
+++ src/main/java/com/cocodan/triplan/aop/aspect/LogTraceAspect.java
@@ -0,0 +1,35 @@
+package com.cocodan.triplan.aop.aspect;
+
+import com.cocodan.triplan.aop.logtrace.LogTrace;
+import com.cocodan.triplan.aop.logtrace.TraceStatus;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("com.cocodan.triplan.aop.aspect.Pointcuts.allDomain()")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            Object result = joinPoint.proceed();
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
